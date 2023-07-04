e43ecef1ab6c51d6d244e848e8f32195fb87f3cf
Mehdi Salimi <Salimi.devop@gmail.com>
2023-07-05 02:15:03 +0330

    create footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Header from './components/header/Header';
 import Content from './components/contentContainer/Content';
+import Footer from './components/footer/Footer';
 
 function App() {
     return (
-        <div className='h-screen w-full bg-secondary-color-blue dark:bg-primary-color-blue overflow-hidden dark:text-secondary-color-blue'>
+        <div className='h-screen w-full flex flex-col justify-between bg-secondary-color-blue dark:bg-primary-color-blue overflow-hidden dark:text-secondary-color-blue'>
             <header>
                 <Header />
             </header>
-            <main className='w-full h-screen flex flex-col justify-center text-center'>
+            <main>
                 <Content />
             </main>
+            <footer>
+                <Footer />
+            </footer>
         </div>
     );
 }
--- src/components/contentContainer/DataInput.js
+++ src/components/contentContainer/DataInput.js
@@ -3,7 +3,7 @@ import LoadingSvg from '../../assets/svgIcons/LoadingSvg';
 
 const DataInput = () => {
     return (
-        <form onSubmit={'submitHandler'} className='h-9 flex flex-col items-center gap-4 mx-4 xl:flex-row xl:mx-72'>
+        <form onSubmit={'submitHandler'} className='h-9 flex flex-col items-center gap-5 mx-4 xl:flex-row xl:mx-72'>
             <input type='text' placeholder='Enter your data url' className='input' />
             <button disabled type='button' className='btn'>
                 <LoadingSvg />
