897b2aba95dc325b4652e3c74b9db1bd625a01c0
Mehdi Salimi <Salimi.devop@gmail.com>
2023-07-04 22:55:36 +0330

    add logo to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,13 @@
-import DarkMode from "./DarkMode";
+import DarkMode from './DarkMode';
+import logo from '../../assets/logo.png';
 
 const Header = () => {
     return (
-        <div className="h-6 w-full flex justify-between">
-            <h2>Logo</h2>
-            <DarkMode/>
+        <div className='h-12 w-full flex justify-between'>
+            <div className='w-20 h-fit p-2'>
+                <img src={logo} alt='logo' />
+            </div>
+            <DarkMode />
         </div>
     );
 };
