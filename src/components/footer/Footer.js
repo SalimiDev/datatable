82fb64a909ad0783d1b25930821b9d632fa19c10
Mehdi Salimi <Salimi.devop@gmail.com>
2023-07-07 05:05:41 +0330

    fix some issues

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className='bg-white rounded-lg shadow dark:bg-gray-900 m-4'>
             <div className='w-full max-w-screen-xl mx-auto p-4 md:py-8'>
                 <div className='sm:flex sm:items-center sm:justify-between'>
-                    <a href='/#' className='flex items-center mb-4 sm:mb-0'>
+                    <a href='/' className='flex items-center mb-4 sm:mb-0'>
                         <img src={footerLogo} className='h-8 mr-3' alt='Flowbite Logo' />
                         <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
                             Paya Holding
@@ -14,22 +14,22 @@ const Footer = () => {
                     </a>
                     <ul className='flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400'>
                         <li>
-                            <a href='/#' className='mr-4 hover:underline md:mr-6 '>
+                            <a href='/' className='mr-4 hover:underline md:mr-6 '>
                                 About
                             </a>
                         </li>
                         <li>
-                            <a href='/#' className='mr-4 hover:underline md:mr-6'>
+                            <a href='/' className='mr-4 hover:underline md:mr-6'>
                                 Privacy Policy
                             </a>
                         </li>
                         <li>
-                            <a href='/#' className='mr-4 hover:underline md:mr-6 '>
+                            <a href='/' className='mr-4 hover:underline md:mr-6 '>
                                 Licensing
                             </a>
                         </li>
                         <li>
-                            <a href='/#' className='hover:underline'>
+                            <a href='/' className='hover:underline'>
                                 Contact
                             </a>
                         </li>
--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
     return (
         <nav className='bg-white border-gray-200 dark:bg-gray-900'>
             <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
-                <a href='/#' className='flex items-center'>
+                <a href='/' className='flex items-center'>
                     <img src={headerLogo} className='h-10 mr-3' alt='Paya Holding' />
                     <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>Paya</span>
                 </a>
@@ -37,23 +37,23 @@ const Navbar = () => {
                     <ul className='flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
                         <li>
                             <a
-                                href='/#'
+                                href='/'
                                 className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'>
                                 Home
                             </a>
                         </li>
                         <li>
-                            <a href='/#' className='navlist-item'>
+                            <a href='/' className='navlist-item'>
                                 About
                             </a>
                         </li>
                         <li>
-                            <a href='/#' className='navlist-item'>
+                            <a href='/' className='navlist-item'>
                                 Services
                             </a>
                         </li>
                         <li>
-                            <a href='/#' className='navlist-item'>
+                            <a href='/' className='navlist-item'>
                                 Contact
                             </a>
                         </li>
