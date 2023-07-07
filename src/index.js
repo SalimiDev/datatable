9c4820f14054c1a2d1aeee1c915c8d95925cb15c
Mehdi Salimi <Salimi.devop@gmail.com>
2023-07-07 04:05:45 +0330

    fix routing issue

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import DarkModeContext from './context/DarkModeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <DarkModeContext>
             <App />
         </DarkModeContext>
-    </BrowserRouter>,
+    </HashRouter>,
 );
